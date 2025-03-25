5190bed63418e614c5242fb05e4bcf12d02026f4
Igor Baratta <ialmeidabara@nvidia.com>
2025-03-25 07:42:54 -0700

    mprove mass

--- test_vector_add/mass_example.cu
+++ test_vector_add/mass_example.cu
@@ -4,51 +4,64 @@
 #include <cuda_runtime_api.h>
 #include <cublasdx.hpp>
 
+using value_type = double;
+
+constexpr int P = 3;
+constexpr int num_dofs = (P + 1) * (P + 2)* (P + 3) / 6;
+constexpr int batch_size = 16;
+
+constexpr int m = num_dofs;
+constexpr int n = batch_size;
+constexpr int k = num_dofs;
+
+constexpr int num_elements = 16 * 16 * 16;
 
 template<class GEMM>
-__global__ void gemm_kernel_shared(const typename GEMM::c_value_type  alpha,
-                                   const typename GEMM::a_value_type* a,
-                                   const typename GEMM::b_value_type* b,
-                                   const typename GEMM::c_value_type  beta,
-                                   typename GEMM::c_value_type* c) {
+__global__ void gemm_kernel_shared(const value_type* phi,
+                                    const value_type* u,
+                                    const value_type* c,
+                                    const value_type  alpha,
+                                    const value_type  beta,
+                                    value_type*       output,
+                                    size_t num_dofs) {
     extern __shared__ __align__(16) char smem[];
 
-    // Make global memory tensor
-    auto a_global_tensor = cublasdx::make_tensor(a, GEMM::get_layout_gmem_a());
-    auto b_global_tensor = cublasdx::make_tensor(b, GEMM::get_layout_gmem_b());
-    auto c_global_tensor = cublasdx::make_tensor(c, GEMM::get_layout_gmem_c());
+    //     // Get block index
+    const int block_idx = blockIdx.x;
 
-    // Make shared memory tensor
-    auto [smem_a, smem_b, smem_c] = cublasdx::slice_shared_memory<GEMM>(smem);
+    const size_t u_offset = block_idx * batch_size * num_dofs;
+    const size_t c_offset = block_idx * batch_size * num_dofs;
+
+    auto phi_tensor = cublasdx::make_tensor(phi, GEMM::get_layout_gmem_a());
+    auto u_tensor = cublasdx::make_tensor(u + u_offset, GEMM::get_layout_gmem_b());
+    auto c_tensor = cublasdx::make_tensor(c + c_offset, GEMM::get_layout_gmem_c());
+
+    auto [smem_a, smem_b] = cublasdx::slice_shared_memory_ab<GEMM>(smem);
     auto a_shared_tensor = cublasdx::make_tensor(smem_a, GEMM::get_layout_smem_a());
     auto b_shared_tensor = cublasdx::make_tensor(smem_b, GEMM::get_layout_smem_b());
-    auto c_shared_tensor = cublasdx::make_tensor(smem_c, GEMM::get_layout_smem_c());
 
-    // Load data from global memory tensor to shared memory tensor
     using alignment = cublasdx::alignment_of<GEMM>;
-    cublasdx::copy<GEMM, alignment::a>(a_global_tensor, a_shared_tensor);
-    cublasdx::copy<GEMM, alignment::b>(b_global_tensor, b_shared_tensor);
-    cublasdx::copy<GEMM, alignment::c>(c_global_tensor, c_shared_tensor);
+    
+    // First GEMM: phi * U
+    cublasdx::copy<GEMM, alignment::a>(phi_tensor, a_shared_tensor);
+    cublasdx::copy<GEMM, alignment::b>(u_tensor, b_shared_tensor);
     cublasdx::copy_wait();
 
-    // Execute GEMM
-    GEMM().execute(alpha, a_shared_tensor, b_shared_tensor, beta, c_shared_tensor);
-    __syncthreads();
+    auto [c_frag, partitioner] = GEMM().execute(a_shared_tensor, b_shared_tensor);
 
-    // Store data from shared memory tensor to global memory tensor
-    cublasdx::copy<GEMM, alignment::c>(c_shared_tensor, c_global_tensor);
+    // Second GEMM: phi^T * (phi * U)
+    auto d_frag = partitioner.make_accumulator_fragment();
+    cublasdx::copy_fragment<alignment::c>(c_tensor, d_frag, partitioner);
+    cublasdx::axpby(alpha, c_frag, beta, d_frag);
+
+    // Copy result back to global memory
+    auto out_global_tensor = cublasdx::make_tensor(output + c_offset, GEMM::get_layout_gmem_c());
+    cublasdx::copy_fragment<alignment::c>(d_frag, out_global_tensor, partitioner);
 }
 
 
 
 int main(int, char**) {
-
-    using value_type = double;
-
-    constexpr int m = 32;
-    constexpr int n = 32;
-    constexpr int k = 32;
-
     constexpr int block_size = 256;
     constexpr int Arch = 800;
     
@@ -64,8 +77,11 @@ int main(int, char**) {
 
 
     constexpr auto global_a_size = m * k;
-    constexpr auto global_b_size = k * n;
-    constexpr auto global_c_size = m * n;
+    constexpr auto global_b_size = num_elements * k * n;
+    constexpr auto global_c_size = num_elements * m * n;
+
+    // U = phi^T * phi * U
+
 
     value_type* a;
     value_type* b;
@@ -82,7 +98,12 @@ int main(int, char**) {
     for (int i = 0; i < global_b_size; ++i) {
         b[i] = static_cast<value_type>(1);
     }
-    gemm_kernel_shared<GEMM><<<1, GEMM::block_dim, cublasdx::get_shared_storage_size<GEMM>()>>>(1.0, a, b, 1.0, c);
+
+    constexpr auto block_dim = 256;
+    constexpr auto grid_size = (global_c_size + block_dim - 1) / block_dim;
+    gemm_kernel_shared<GEMM><<<grid_size, block_dim, cublasdx::get_shared_storage_size<GEMM>()>>>(
+        a, b, c, 1.0, 0.0, c, num_dofs
+    );
     cudaPeekAtLastError();
 
     cudaDeviceSynchronize();
